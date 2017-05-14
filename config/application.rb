957269cc6628bc01cb2d73bca135c81d5a7dbdd2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-14 22:05:27 +0900

    [config] config.generators の変更

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,10 @@ module TamachiVue
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |g|
+      g.helper false
+      g.assets false
+      g.skip_routes true
+    end
   end
 end
