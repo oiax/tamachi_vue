be0a387d3116c29a05dbff27ee45da11f7ef1549
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-14 22:27:30 +0900

    [db] users テーブル用シードデータ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   validates :name, :language, presence: true
   validates :other_language,
     presence: { if: -> (obj) { obj.language == "other" } }
+
+  before_save do
+    unless language == "other"
+      self.other_language = ""
+    end
+  end
 end
