d4512695b426fc542303224cc99960be23ea465f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-14 22:50:35 +0900

    [users] users#new, edit, create, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,35 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to action: "index"
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to action: "index"
+    else
+      render action: "edit"
+    end
+  end
+
+  private def user_params
+    params.require(:user).permit(
+      :name, :language, :other_language
+    )
   end
 end
