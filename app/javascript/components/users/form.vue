8203f2d8c6e743846e146e48f31f4a0935be50d9
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-22 00:17:13 +0900

    [vue] SFC 化

---
+++ app/javascript/components/users/form.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="user-form" v-cloak>
+    <form>
+      <div class="form-group">
+        <label for="user_name">お名前</label>
+        <input class="form-control" style="width: 300px" v-model="user.name"
+          type="text" name="user[name]">
+      </div>
+      <div class="form-group">
+        <label for="user_language">もっとも得意なプログラミング言語</label>
+        <div>
+          <label>
+            <input v-model="user.language" type="radio" value="ruby"
+              name="user[language]"> Ruby
+          </label>
+          <label>
+            <input v-model="user.language" type="radio" value="php"
+              name="user[language]"> PHP
+          </label>
+          <label>
+            <input v-model="user.language" type="radio" value="other"
+              name="user[language]"> その他
+          </label>
+        </div>
+      </div>
+      <div class="form-group" v-show="user.language === 'other'">
+        <label for="user_other_language">（具体的に）</label>
+        <input style="display: inline; width: auto" class="form-control"
+          v-model="user.other_language" type="text" name="user[other_language]">
+      </div>
+
+      <input type="submit" name="commit" value="登録" class="btn btn-primary">
+    </form>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      user: {
+        name: "",
+        language: "",
+        other_language: ""
+      }
+    }
+  }
+}
+</script>
--- app/javascript/packs/users/form.js
+++ app/javascript/packs/users/form.js
@@ -1,21 +1,12 @@
 import Vue from 'vue/dist/vue.esm'
+import UsersForm from '../../components/users/form.vue'
 
 Vue.config.productionTip = false
 
 document.addEventListener("DOMContentLoaded", () => {
-  const language = document.querySelector("[v-model='user.language']:checked")
-
   new Vue({
     el: "#user-form",
-    data: function () {
-      return {
-        user: {
-          name: document.querySelector("[v-model='user.name']").value,
-          language: language ? language.value : undefined,
-          other_language: document
-            .querySelector("[v-model='user.other_language']").value
-        }
-      }
-    }
+    template: "<UsersForm/>",
+    components: { UsersForm }
   })
 })
