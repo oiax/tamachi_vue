09bde7d44945628cdc18b9a19559713bda871e97
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-05-29 13:09:08 +0900

    [vue, erb] Use bootstrap_form_for and Turbolinks

--- app/javascript/packs/users/form.js
+++ app/javascript/packs/users/form.js
@@ -1,11 +1,13 @@
 import Vue from "vue/dist/vue.esm"
+import TurbolinksAdapter from 'vue-turbolinks'
 import VueDataScooper from "vue-data-scooper"
 
 Vue.config.productionTip = false
 Vue.use(VueDataScooper)
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('turbolinks:load', () => {
   new Vue({
-    el: "#user-form"
+    el: "#user-form",
+    mixins: [TurbolinksAdapter]
   })
 })
